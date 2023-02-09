793aae9a652e9f34f863150972a281c064d5259a
Mike <52970344+Developer-Mike@users.noreply.github.com>
2023-02-09 17:42:41 +0100

    No crash on low wifi speeds

--- helpers/post_helper.py
+++ helpers/post_helper.py
@@ -24,10 +24,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-if constants.VIRTUAL_DISPLAY: 
-    from pyvirtualdisplay import Display
-else:
-    from contextlib import nullcontext
+if constants.VIRTUAL_DISPLAY: from pyvirtualdisplay import Display
+else: from contextlib import nullcontext
 
 def _post_twitter(image, caption):
     with Display(visible=False, size=(800, 600)) if constants.VIRTUAL_DISPLAY else nullcontext() as _:
@@ -37,29 +35,29 @@ def _post_twitter(image, caption):
         driver.get("https://twitter.com/i/flow/login")
         driver.maximize_window()
 
-        email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"]')))
+        email_field = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"]')))
         email_field.send_keys(constants.TWITTER_EMAIL)
         email_field.send_keys(Keys.ENTER)
 
         # Security username question
         try:
-            security_username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"][@autocapitalize="none"]')))
+            security_username_field = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"][@autocapitalize="none"]')))
             security_username_field.send_keys(constants.TWITTER_USERNAME)
             security_username_field.send_keys(Keys.ENTER)
         except:
             pass
 
-        password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@type="password"]')))
+        password_field = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '//input[@type="password"]')))
         password_field.send_keys(constants.TWITTER_PASSWORD)
         password_field.send_keys(Keys.ENTER)
 
         # Logged in
 
         if image != None:
-            post_image_button = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//input[@type="file"]')))
+            post_image_button = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '//input[@type="file"]')))
             post_image_button.send_keys(image)
 
-        post_caption_textarea = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//div[@role="textbox"][@contenteditable="true"]')))
+        post_caption_textarea = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '//div[@role="textbox"][@contenteditable="true"]')))
         post_caption_textarea.send_keys(caption.replace("\\n", Keys.ENTER))
         driver.implicitly_wait(5)
         post_caption_textarea.send_keys(Keys.CONTROL, Keys.ENTER)
