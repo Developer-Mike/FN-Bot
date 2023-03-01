c37cd7e32412cff172401773dc0bbf5e396b73bf
Mike <52970344+Developer-Mike@users.noreply.github.com>
2023-03-01 19:56:25 +0100

    Added option to post when some images don't load.

--- constants.py
+++ constants.py
@@ -11,6 +11,7 @@ dotenv.load_dotenv(os.path.join(BASE_PATH, ".env"))
 
 LANG = os.getenv("LANG")
 FORTNITEAPI_IO_KEY = os.getenv("FORTNITEAPI_IO_KEY")
+CONTINUE_ON_CORRUPTED_IMAGE = os.getenv("CONTINUE_ON_CORRUPTED_IMAGE") in ("true", "1")
 
 VIRTUAL_DISPLAY = os.getenv("VIRTUAL_DISPLAY") in ("true", "1")
 CHROMEDRIVER_PATH = os.path.join(BASE_PATH, os.getenv("CHROMEDRIVER_PATH"))
--- helpers/image_helper.py
+++ helpers/image_helper.py
@@ -2,14 +2,20 @@ import requests
 from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 
+import constants
+
 def from_url(url, size=512, mode='RGBA'):
-    response = requests.get(f"{url}?width={size}", stream=True)
-    
-    try: 
+    try:
+        response = requests.get(f"{url}?width={size}", stream=True)
+
         image = Image.open(BytesIO(response.content))
         return image.convert(mode).resize((size, size), Image.Resampling.LANCZOS)
-    except UnidentifiedImageError:
+    except Exception as e:
         print(f"Error: {url} is not a valid image.")
+
+        if not constants.CONTINUE_ON_CORRUPTED_IMAGE or (not isinstance(e, UnidentifiedImageError) and not isinstance(e, requests.exceptions.MissingSchema)):
+            raise e
+
         return Image.new(mode, (size, size), (0, 0, 0, 0))
 
 def from_path(path, size=512):
